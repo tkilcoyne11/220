539208e477e7786008c881749871f33193cb3ab1
tuckerkilcoyne <kilcoyne@g.cofc.edu>
2021-09-01 14:18:32 -0400

    completed lab 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,13 @@
+"""
+Name: Tucker Kilcoyne
+hello.py
+Problem: This program prints hello world to the screen
+Certification of Authenticity
+I certify that this assignment is entirely my own work
+"""
+
+
+def main():
+    print("hello, world!")
+
+main()
