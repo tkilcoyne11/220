cc927b06937182f485d56d08d630c1b78aec80b0
tuckerkilcoyne <kilcoyne@g.cofc.edu>
2021-09-08 14:47:28 -0400

    Completed lab2

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -1,4 +1,51 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Tucker Kilcoyne
+lab2.py
 """
+
+import math
+
+def sum_of_threes():
+    bound = int(input("Enter an upper bound: "))
+    acc = 0
+    for i in range(0,bound+1,3):
+        acc += i # this equals acc = acc + i
+    print(acc)
+
+def multiplication_table():
+    for h in range(1,11):
+        for l in range(1,11):
+            print(h * l, end=" ")
+        print()
+    # /n represents the enter key
+
+def triangle_area():
+    a = float(input("Enter length of a: "))
+    b = float(input("Enter length of b: "))
+    c = float(input("Enter length of c: "))
+    s = (a+b+c)/2
+    x = s*(s-a)*(s-b)*(s-c)
+    area = math.sqrt(x)
+    print(round(area,2))
+
+def sumSquares():
+    start = int(input("Enter starting value: "))
+    end = int(input("Enter ending value: "))
+    acc = 0
+    for i in range(start,end+1):
+        acc += (i*i)
+    print(acc)
+
+def power():
+    base = int(input("Enter the base: "))
+    exponent = int(input("Enter the exponent: "))
+    acc = 1
+    for i in range(exponent):
+        acc *= base
+    print(acc)
+
+#sum_of_threes()
+#multiplication_table()
+#triangle_area()
+#sumSquares()
+#power()
