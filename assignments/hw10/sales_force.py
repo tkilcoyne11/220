9c4610a399b1ac10a722c2371420224ecb05d7b4
tuckerkilcoyne <kilcoynetl@g.cofc.edu>
2021-12-02 23:45:20 -0500

    Homework 10

---
+++ assignments/hw10/sales_force.py
@@ -0,0 +1,53 @@
+"""
+Tucker Kilcoyne
+sales_force.py
+"""
+
+from sales_person import SalesPerson
+
+
+class SalesForce:
+    """
+    This class creates a group of sales people
+    """
+
+    def __init__(self):
+        self.sales_people = []
+
+    def add_data(self, file_name):
+        file = open(file_name, "r")
+        for i in file.readline():
+            variables = i.split(",")
+            employee = SalesPerson(int(variables[0]), variables[1])
+            for j in variables[2].split():
+                employee.enter_sales(float(j))
+            self.sales_people.append(employee)
+
+    def quota_report(self, quota):
+        report = []
+        for employee in self.sales_people:
+            data = [employee.get_id(), employee.get_name(),
+                    employee.total_sales, employee.met_quota(quota)]
+            report.append(data)
+        return report
+
+    def top_seller(self):
+        top_seller = []
+        for i in range(len(self.sales_people)):
+            for j in range(i+1, len(self.sales_people)):
+                if self.sales_people[j].compare_to(self.sales_people[i]) > 0:
+                    self.sales_people[i], self.sales_people[j] = \
+                        self.sales_people[j], self.sales_people[i]
+        top_seller.append(self.sales_people[0])
+        for i in range(1, len(self.sales_people)):
+            if self.sales_people[i] >= top_seller[0].total_sales():
+                top_seller.append(self.sales_people[i])
+            else:
+                break
+        return top_seller
+
+    def individual_sales(self, employee_id):
+        for i in range(len(self.sales_people)):
+            if self.sales_people[i] == employee_id:
+                return self.sales_people[i]
+        return None
---
+++ assignments/hw10/sales_person.py
@@ -0,0 +1,51 @@
+"""
+Tucker Kilcoyne
+sales_person.py
+"""
+
+
+class SalesPerson:
+    """
+    This class creates employee data
+    """
+
+    def __init__(self, employee_id, name):
+        self.employee_id = employee_id
+        self.name = name
+        self.sales = []
+
+    def get_id(self):
+        return self.employee_id
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def enter_sales(self, sale):
+        self.sales.append(float(sale))
+
+    def total_sales(self):
+        acc = 0
+        for i in self.sales:
+            acc += i
+        return acc
+
+    def get_sales(self):
+        return self.sales
+
+    def met_quota(self, quota):
+        if self.total_sales() > quota:
+            return True
+        return False
+
+    def compare_to(self, other):
+        if self.total_sales() == other.total_sales:
+            return 0
+        elif self.total_sales() > other.total_sales:
+            return 1
+        return -1
+
+    def __str__(self):
+        return str(self.get_id() + "-" + self.name + ":" + str(self.total_sales()))
